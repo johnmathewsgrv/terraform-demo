ce79ee3a533acb3173faa67fffe1ad32f29e36bb
johnmathewsgrv <johnmathewsgrv@gmail.com>
2026-04-14 18:26:24 +0530

    Create main.tf

---
+++ environments/test/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = "${var.project_id}-test-bucket"
+  location      = var.region
+  force_destroy = true
+}
