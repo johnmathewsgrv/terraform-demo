1521fadb79ea388d7ba8cbb6873e0191ffb0a1d7
johnmathewsgrv <johnmathewsgrv@gmail.com>
2026-04-14 18:40:36 +0530

    Update main.tf

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -16,4 +16,6 @@ resource "google_storage_bucket" "bucket" {
   name          = "${var.project_id}-test-bucket"
   location      = var.region
   force_destroy = true
+
+  uniform_bucket_level_access = true
 }
